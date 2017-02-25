b7a396bb589abd5f1e4d3099e6a5e96c44b4b9cd
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-02-25 04:46:23 +0100

    Add train and validation set shortcuts

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,1 +1,5 @@
+import pandas as pd
+
 labels = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+train = pd.read_csv('../data/validation/train.csv')
+validation = pd.read_csv('../data/validation/test.csv')
