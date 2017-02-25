1bc707bc6579ccb5c2287a887ba6ca50a480808b
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-02-25 04:47:58 +0100

    Add regular data generator

--- dataset/data_generators.py
+++ dataset/data_generators.py
@@ -4,8 +4,57 @@ import numpy as np
 from keras.utils.np_utils import to_categorical as onehot
 
 from preprocess import *
+from dataset import labels
 
-def get_data_with_masks(data_df, bboxes, data_folder, labels, batch_size=1,
+def get_data(data_df, data_folder, batch_size=32, shuffle=True, 
+             augmentation=True, img_size=(256, 256), **kwargs):
+    """
+    Generator to train a model on images.
+
+    # Params
+    - data_df : DataFrame of filename and label for each image
+    - data_folder : folder where data resides. Should have structure 
+                    `data_folder/label/img_name`
+    - batch_size : number of images per batch
+    - shuffle : present images in random order (each epoch)
+    - augmentation : perform data augmentation
+    - img_size : sample patches of this size from the image and mask
+    - kwargs : passed to the preprocess function
+
+    # Returns
+    - batch of images (batch_size, 3, img_size[0], img_size[1])
+    """
+    n_classes = len(labels)
+    
+    while True:
+        data = zip(data_df.filename.values, data_df.label.values)
+        n = len(data)
+        if shuffle:
+            data = np.random.permutation(data)
+        data = list(data)
+
+        # Double to allow for larger batch sizes
+        data += data
+        i = 0
+        while i < n:
+            img_batch = np.zeros((batch_size, 3) + img_size, dtype=np.float32)
+            label_batch = np.zeros((batch_size, n_classes), dtype=np.uint8)
+            for j in range(batch_size):
+                img_name, label = data[i + j]
+                img_path = os.path.join(data_folder, label, img_name)
+                img = load_image(img_path)
+                
+                img = preprocess(img, target_size=img_size, 
+                                augmentation=augmentation, 
+                                zero_center=True, scale=1./255.,
+                                **kwargs)
+                
+                img_batch[j] = img
+                label_batch[j] = onehot(labels.index(label), 8)
+                i += 1
+            yield img_batch, label_batch
+
+def get_data_with_masks(data_df, bboxes, data_folder, batch_size=1,
                         shuffle=True, augmentation=True, img_size=(256, 256),
                         **kwargs):
     """
@@ -18,7 +67,6 @@ def get_data_with_masks(data_df, bboxes, data_folder, labels, batch_size=1,
                img_name -> [(x, y, width, height, class), ...]
     - data_folder : folder where data resides. Should have structure 
                     `data_folder/label/img_name`
-    - labels : list of label names
     - batch_size : number of images per batch
     - shuffle : present images in random order (each epoch)
     - augmentation : perform data augmentation
@@ -26,7 +74,7 @@ def get_data_with_masks(data_df, bboxes, data_folder, labels, batch_size=1,
     - kwargs : passed to the preprocess function
 
     # Returns
-    - batch of images (batch_size, 3, img_size[0], img_size[1]
+    - batch of images (batch_size, 3, img_size[0], img_size[1])
     - a list of: 
             1. labels (batch_size, n_classes)
             2. masks (batch_size, 1, img_size[0], img_size[1])
