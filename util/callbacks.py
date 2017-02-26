d90841af2e475a7a2a16cfea33b10d50c32eabdb
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2017-02-26 03:58:13 +0100

    Add plotting for 1 output models

--- util/callbacks.py
+++ util/callbacks.py
@@ -25,25 +25,39 @@ class ShowSegmentation(Callback):
         self.n_samples = n_samples
 
     def on_epoch_end(self, epoch, logs):
-        for _ in range(self.n_samples):
-            img, [label, mask] = next(self.data_gen)
-            img, label, mask = img[0], label[0], mask[0]
+        i = 0
+        for img_batch, y_batch in self.data_gen:
+            for img, y in zip(img_batch, y_batch):
+                if type(y) is tuple:
+                    label, mask = y
+                else:
+                    label = None
+                    mask = y
+                plt.figure(figsize=(13, 7))
+                plt.subplot(131)
+                plt.title('Input patch')
+                plt.imshow(img.transpose(1, 2, 0) + 0.5)
 
-            plt.subplot(131)
-            plt.title('Input patch')
-            plt.imshow(img.transpose(1, 2, 0) + 0.5)
+                plt.subplot(132)
+                plt.title('Target')
+                plt.imshow(mask[1])
 
-            plt.subplot(132)
-            plt.title('Target')
-            plt.imshow(mask.squeeze())
+                plt.subplot(133)
+                plt.title('Segmentation')
+                batch_img = np.expand_dims(img, 0)
+                pred = self.model.predict(batch_img, verbose=0)
+                if type(pred) is tuple:
+                    pred, segm = pred
+                else:
+                    segm = pred
+                plt.imshow(segm[0, 1])
+                plt.show()
 
-            plt.subplot(133)
-            plt.title('Segmentation')
-            batch_img = np.expand_dims(img, 0)
-            pred, segm = self.model.predict(batch_img, verbose=0)
-            plt.imshow(segm[0].squeeze())
-            plt.show()
+                if label is not None:
+                    print 'Label:', label
+                    print 'Predicted:', pred[0]
+                i += 1
+                if i >= self.n_samples:
+                    return
 
-            print 'Label:', label
-            print 'Predicted:', pred[0]
             
