097432d00a462d73bb9f1bdfad2d9182d3822b31
Thomas de Bel <debel.thomas@gmail.com>
2017-03-29 09:08:14 +0200

    added patchgenerator (wip)

---
+++ PatchGenerator/patchbuffer.py
@@ -0,0 +1,77 @@
+import numpy as np
+import threading
+
+class PatchBuffer(object):
+	def __init__(self, patch_shape, label_shape, cache_size, mode = 'ring'):
+		'''
+		'''
+		
+		super(PatchBuffer, self).__init__()
+		
+		self._mode = mode
+		self._read_index = 0
+        self._write_index = 0
+        self._buffer_size = cache_size
+		self._patch_buffer = None
+		self._label_buffer = None
+		
+		
+		self._allocate_space(patch_shape, label_shape, cache_size)
+		self._set_mode(mode)
+		self._buffer_lock = threading.Lock()
+		
+	def _allocate_space(self, patch_shape, label_shape, cache_size):
+		'''
+		'''
+		if patch_shape[0] <= 0 or patch_shape[1] <= 0:
+			raise ValueError('Invalid patch shape ({},{})'.format(patch_shape[0], patch_shape[1]))
+		
+		self._patch_buffer = np.zeros((cache_size, 3, patch_shape[0], patch_shape[1]), dtype=np.uint8)
+		if label_shape is not None:
+			self._label_buffer = np.zeros((cache_size, 1, label_shape[0], label_shape[1]), dtype=np.uint8)
+		else:
+			self._label_buffer = np.zeros((cache_size,), dtype=np.uint8)
+			
+	def _apply_mode(self, mode):
+		'''
+		'''
+		#tbd: random mode
+		
+		
+	def get_batch(self, patch_count):
+		'''
+		'''
+		self._buffer_lock.acquire()
+		
+		if self._mode == 'random':
+			raise NotImplementedError('random mode not implemented yet')
+		elif self._mode == 'ring':
+			indices = np.arange(self._read_index, self._read_index + patches.shape[0])
+			indices %= self._buffer_size
+			self._read_index = (self._read_index + patches.shape[0]) % self._buffer_size
+			
+		labels = self._label_buffer[indices]
+		patches = self._patch_buffer[indices]
+		
+		self._buffer_lock.release()
+		
+		return patches, labels, indices
+		
+	def push_patches(self, patches, labels):
+		'''
+		'''
+		self._buffer_lock.acquire()
+		
+		if self._mode == 'random':
+			raise NotImplementedError('random mode not implemented yet')
+		elif self._mode == 'ring':
+			indices = np.arange(self._write_index, self._write_index + patches.shape[0])
+			indices %= self._buffer_size
+			self._write_index = (self._write_index + patches.shape[0]) % self._buffer_size
+			
+		self._label_buffer[indices] = labels
+		self._patch_buffer[indices] = patches
+		
+		self._buffer_lock.release()
+			
+		
---
+++ PatchGenerator/patchgenerator.py
@@ -0,0 +1,98 @@
+from . import patchbuffer as buffer
+from . import patchsampler as sampler
+
+import threading
+import numpy as np
+import Queue
+
+
+class PatchGenerator(object):
+
+	def __init__(self, label_list, main_cache_size, read_size, patch_shape, label_shape, buffer_mode, augmentation_ratio, augmentation_list, sampling_mode, multi_threaded): 
+		'''
+		'''
+		
+		super(PatchGenerator, self).__init__()
+		
+		self._cache_size = 0
+		self._read_size = 0
+		self._patch_shape = (0,0)
+		self._label_shape = None
+		self._label_list = []
+		self._multi_threaded = False
+		self._augmentation_ratio = 0.
+		self._patch_buffer = None
+		self._read_buffer = None
+		
+		
+		self._init_buffers(main_cache_size, read_size, patch_shape, label_shape, buffer_mode)
+		self._init_sampler(patch_source, label_list, augmentation_ratio, augmentation_list, sampling_mode, multi_threaded)
+		
+	
+	def _init_buffers(self, main_cache_size, read_size, patch_shape, label_shape, buffer_mode):
+		'''
+		Initiate main buffer and read buffer if double buffering.
+		'''
+		
+		self._cache_size = main_cache_size
+		self._patch_shape = patch_shape
+		self._label_shape = label_shape
+		
+		self._patch_buffer = buffer.PatchBuffer(self._patch_shape, self._label_shape, self._cache_size, buffer_mode)
+        
+        if read_size is not None:
+			self._read_size = read_size
+			self._read_buffer = buffer.PatchBuffer(self._patch_shape, self._label_shape, self._read_size, buffer_mode)
+
+	def _init_sampler(patch_source, label_list, augmentation_ratio, augmentation_list, multi_threaded):
+		'''
+		'''
+		self._multi_threaded = multi_threaded
+		self._label_list = label_list
+		self._augmentation_ratio = augmentation_ratio
+		
+		if self._multi_threaded:
+			raise NotImplementedError('multi-threading mode not implemented yet')
+		else:
+			self._patch_sampler = sampler.PatchSampler(patch_source, self._patch_shape, self._label_shape, self._label_list, self._augmentation_ratio, augmentation_list, sampling_mode)
+		
+		
+		
+	def _load(self, batch_size, buffer):
+		'''
+		Fill the target buffer with a batch of patches.
+		'''
+		
+		patches, labels = self._patch_sampler.gen_batch(self._augmentation_ratio)
+		buffer.push(patches, labels)
+		
+	def load(self,batch_size):
+		'''
+		'''
+		
+		if self._read_buffer:
+			self._load(batch_size, self._read_buffer)
+		else:
+			self._load(batch_size, self._patch_buffer)
+			
+	def transfer(self):
+		'''
+		'''
+		
+		patches, labels = self._read_buffer.get_batch(self._read_size)
+		self._patch_buffer.push_patches(patches, labels)
+		
+	def batch(self, batch_size):
+		'''
+		'''
+		
+		patches, labels, indices = self._patch_buffer.get_batch(batch_size)
+		
+		patches = patches.astype(float32) / 255.0
+		
+		return patches, labels, indices
+		
+		
+		
+		
+		
